e964cd1abe5c341dc5781bceb053d758cd055142
jbmaigrot <jb.maigrot@neuf.fr>
2017-05-09 17:15:33 +0200

    merge fix (added TestJBDC.java and ConnectBDD.java)

---
+++ komodo/src/com/komodo/bdd/ConnectBDD.java
@@ -0,0 +1,42 @@
+package com.komodo.bdd;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.sql.Connection;
+
+public class ConnectBDD {
+    public Connection getConnection() {
+	    /* Chargement du driver JDBC pour MySQL */
+	    try {
+	        System.out.println( "Chargement du driver..." );
+	        Class.forName( "com.mysql.jdbc.Driver" );
+	        System.out.println( "Driver chargé !" );
+	    } catch ( ClassNotFoundException e ) {
+	    	System.out.println( "Erreur lors du chargement : le driver n'a pas été trouvé dans le classpath ! <br/>"
+	                + e.getMessage() );
+	    }
+
+	    /* Connexion à la base de données */
+	    String url = "jdbc:mysql://localhost/Komodo";
+	    String utilisateur = "root";
+	    String motDePasse = "";
+	    Connection connexion=null;
+	    try {
+	    	System.out.println( "Connexion à la base de données..." );
+	        connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+	        System.out.println( "Connexion réussie !" );
+	       
+	    } catch ( SQLException e ) {
+	    	System.out.println( "Erreur lors de la connexion : <br/>"
+	                + e.getMessage() );
+	    } 
+	    return connexion;
+    }
+}
---
+++ komodo/src/com/komodo/bdd/TestJDBC.java
@@ -0,0 +1,88 @@
+package com.komodo.bdd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TestJDBC {
+    /* La liste qui contiendra tous les résultats de nos essais */
+    private List<String> messages = new ArrayList<String>();
+
+    public List<String> executerTests( HttpServletRequest request ) {
+    	    /* Chargement du driver JDBC pour MySQL */
+    	    try {
+    	        messages.add( "Chargement du driver..." );
+    	        Class.forName( "com.mysql.jdbc.Driver" );
+    	        messages.add( "Driver chargé !" );
+    	    } catch ( ClassNotFoundException e ) {
+    	        messages.add( "Erreur lors du chargement : le driver n'a pas été trouvé dans le classpath ! <br/>"
+    	                + e.getMessage() );
+    	    }
+
+    	    /* Connexion à la base de données */
+    	    String url = "jdbc:mysql://cpg.sharethepool.ovh:443/komodo";
+    	    String utilisateur = "komodo";
+    	    String motDePasse = "AlxK8*AnU";
+    	    Connection connexion = null;
+    	    Statement statement = null;
+    	    ResultSet resultat = null;
+    	    try {
+    	        messages.add( "Connexion à la base de données..." );
+    	        connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+    	        messages.add( "Connexion réussie !" );
+
+    	        /* Création de l'objet gérant les requêtes */
+    	        statement = connexion.createStatement();
+    	        messages.add( "Objet requête créé !" );
+
+    	        /* Exécution d'une requête de lecture */
+    	        resultat = statement.executeQuery( "SELECT * FROM Grille_de_competence_APP;" );
+    	        messages.add( "Requête \"SELECT * FROM Grille_de_competence_APP;\" effectuée !" );
+    	 
+    	        /* Récupération des données du résultat de la requête de lecture */
+    	        while ( resultat.next() ) {
+    	            int idGrille = resultat.getInt( "id_grille" );
+    	            String nomGrille = resultat.getString( "Nom_grille" );
+    	            String promo = resultat.getString( "Promo" );
+    	         
+    	            /* Formatage des données pour affichage dans la JSP finale. */
+    	            messages.add( "Données retournées par la requête : id = " + idGrille + ", nom grille = " + nomGrille
+    	                    + ", promo = "
+    	                    + promo + "." );
+    	        }
+    	    } catch ( SQLException e ) {
+    	        messages.add( "Erreur lors de la connexion : <br/>"
+    	                + e.getMessage() );
+    	    } finally {
+    	        messages.add( "Fermeture de l'objet ResultSet." );
+    	        if ( resultat != null ) {
+    	            try {
+    	                resultat.close();
+    	            } catch ( SQLException ignore ) {
+    	            }
+    	        }
+    	        messages.add( "Fermeture de l'objet Statement." );
+    	        if ( statement != null ) {
+    	            try {
+    	                statement.close();
+    	            } catch ( SQLException ignore ) {
+    	            }
+    	        }
+    	        messages.add( "Fermeture de l'objet Connection." );
+    	        if ( connexion != null ) {
+    	            try {
+    	                connexion.close();
+    	            } catch ( SQLException ignore ) {
+    	            }
+    	        }
+    	    }
+
+    	    return messages;
+    	}
+}
