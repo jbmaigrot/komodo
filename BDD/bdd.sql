0b985bae6d7511f09450eb279aee920de84d16a1
UBotella <ugo.botella@isep.fr>
2017-05-16 16:31:48 +0200

    Ajout valuer de base

--- BDD/bdd.sql
+++ BDD/bdd.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 09, 2017 at 03:15 PM
+-- Generation Time: May 16, 2017 at 04:30 PM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
--- PHP Version: 7.0.15-0ubuntu0.16.04.4
+-- PHP Version: 5.6.30-10+deb.sury.org~xenial+2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `Appartient`
 --
 
+DROP TABLE IF EXISTS `Appartient`;
 CREATE TABLE `Appartient` (
   `idEleve` int(10) UNSIGNED NOT NULL,
   `idGroupe` int(10) UNSIGNED NOT NULL,
@@ -33,23 +34,35 @@ CREATE TABLE `Appartient` (
   `datecrea` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Appartient`
+--
+
+TRUNCATE TABLE `Appartient`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Competence_principale`
 --
 
+DROP TABLE IF EXISTS `Competence_principale`;
 CREATE TABLE `Competence_principale` (
   `id_comp` int(10) UNSIGNED NOT NULL,
   `Nom_competence_principale` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Competence_principale`
+--
+
+TRUNCATE TABLE `Competence_principale`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Competence_secondaire`
 --
 
+DROP TABLE IF EXISTS `Competence_secondaire`;
 CREATE TABLE `Competence_secondaire` (
   `id_comp_second` int(10) UNSIGNED NOT NULL,
   `id_comp_princ` int(10) UNSIGNED NOT NULL,
@@ -57,12 +70,18 @@ CREATE TABLE `Competence_secondaire` (
   `Nom` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Competence_secondaire`
+--
+
+TRUNCATE TABLE `Competence_secondaire`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Consulte`
 --
 
+DROP TABLE IF EXISTS `Consulte`;
 CREATE TABLE `Consulte` (
   `Evaluation` varchar(45) NOT NULL,
   `CommentaireGroupe` text NOT NULL,
@@ -71,23 +90,35 @@ CREATE TABLE `Consulte` (
   `idCom_Sec_Util` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Consulte`
+--
+
+TRUNCATE TABLE `Consulte`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Critere`
 --
 
+DROP TABLE IF EXISTS `Critere`;
 CREATE TABLE `Critere` (
   `id_critere` int(10) UNSIGNED NOT NULL,
-  `Descriptif` varchar(600) NOT NULL
+  `Descriptif` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Critere`
+--
+
+TRUNCATE TABLE `Critere`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Eleves`
 --
 
+DROP TABLE IF EXISTS `Eleves`;
 CREATE TABLE `Eleves` (
   `id_utilisateur` int(10) UNSIGNED NOT NULL,
   `Numero_eleve` int(10) UNSIGNED NOT NULL,
@@ -95,12 +126,18 @@ CREATE TABLE `Eleves` (
   `Specialite` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Eleves`
+--
+
+TRUNCATE TABLE `Eleves`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Evalue`
 --
 
+DROP TABLE IF EXISTS `Evalue`;
 CREATE TABLE `Evalue` (
   `idEleve` int(10) UNSIGNED NOT NULL,
   `idGroupe` int(10) UNSIGNED NOT NULL,
@@ -111,18 +148,29 @@ CREATE TABLE `Evalue` (
   `CommentaireProfesseur` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Evalue`
+--
+
+TRUNCATE TABLE `Evalue`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Grille_de_competence_APP`
 --
 
+DROP TABLE IF EXISTS `Grille_de_competence_APP`;
 CREATE TABLE `Grille_de_competence_APP` (
   `id_grille` int(10) UNSIGNED NOT NULL,
   `Nom_grille` varchar(45) NOT NULL,
   `Promo` int(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Grille_de_competence_APP`
+--
+
+TRUNCATE TABLE `Grille_de_competence_APP`;
 --
 -- Dumping data for table `Grille_de_competence_APP`
 --
@@ -137,6 +185,7 @@ INSERT INTO `Grille_de_competence_APP` (`id_grille`, `Nom_grille`, `Promo`) VALU
 -- Table structure for table `Groupe`
 --
 
+DROP TABLE IF EXISTS `Groupe`;
 CREATE TABLE `Groupe` (
   `id_groupe` int(10) UNSIGNED NOT NULL,
   `Nom` varchar(45) NOT NULL,
@@ -145,45 +194,69 @@ CREATE TABLE `Groupe` (
   `idGrilleAPP` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Groupe`
+--
+
+TRUNCATE TABLE `Groupe`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie2`
 --
 
+DROP TABLE IF EXISTS `Lie2`;
 CREATE TABLE `Lie2` (
   `idEleve` int(10) UNSIGNED NOT NULL,
   `idGrilleComp` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Lie2`
+--
+
+TRUNCATE TABLE `Lie2`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie3`
 --
 
+DROP TABLE IF EXISTS `Lie3`;
 CREATE TABLE `Lie3` (
   `idGrilleComp` int(10) UNSIGNED NOT NULL,
   `idCompPrin` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Lie3`
+--
+
+TRUNCATE TABLE `Lie3`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie5`
 --
 
+DROP TABLE IF EXISTS `Lie5`;
 CREATE TABLE `Lie5` (
-  `idCompSec` int(10) UNSIGNED NOT NULL,
+  `idCompSecUtil` int(10) UNSIGNED NOT NULL,
   `idCritere` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Lie5`
+--
+
+TRUNCATE TABLE `Lie5`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie6`
 --
 
+DROP TABLE IF EXISTS `Lie6`;
 CREATE TABLE `Lie6` (
   `idGroupe` int(10) UNSIGNED NOT NULL,
   `idPlaning` int(10) UNSIGNED NOT NULL,
@@ -192,28 +265,62 @@ CREATE TABLE `Lie6` (
   `Heure` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Lie6`
+--
+
+TRUNCATE TABLE `Lie6`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Modele_Comp_Prin`
+-- Table structure for table `modele_comp_prin`
 --
 
-CREATE TABLE `Modele_Comp_Prin` (
+DROP TABLE IF EXISTS `modele_comp_prin`;
+CREATE TABLE `modele_comp_prin` (
   `idModCompPrin` int(10) UNSIGNED NOT NULL,
   `Nom` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `modele_comp_prin`
+--
+
+TRUNCATE TABLE `modele_comp_prin`;
+--
+-- Dumping data for table `modele_comp_prin`
+--
+
+INSERT INTO `modele_comp_prin` (`idModCompPrin`, `Nom`) VALUES
+(1, 'Communication'),
+(2, 'Travail en équipe'),
+(3, 'Technique');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Modele_Comp_Sec`
+-- Table structure for table `modele_comp_sec`
 --
 
-CREATE TABLE `Modele_Comp_Sec` (
+DROP TABLE IF EXISTS `modele_comp_sec`;
+CREATE TABLE `modele_comp_sec` (
   `idModCompSec` int(10) UNSIGNED NOT NULL,
-  `ponderation` int(11) NOT NULL,
   `Nom` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `modele_comp_sec`
+--
+
+TRUNCATE TABLE `modele_comp_sec`;
+--
+-- Dumping data for table `modele_comp_sec`
+--
+
+INSERT INTO `modele_comp_sec` (`idModCompSec`, `Nom`) VALUES
+(1, 'Intéragir avec son groupe'),
+(2, 'Communiquer en anglais et en français'),
+(3, 'Savoir écouter et se faire écouter');
 
 -- --------------------------------------------------------
 
@@ -221,17 +328,24 @@ CREATE TABLE `Modele_Comp_Sec` (
 -- Table structure for table `Planning`
 --
 
+DROP TABLE IF EXISTS `Planning`;
 CREATE TABLE `Planning` (
   `id_planning` int(10) UNSIGNED NOT NULL,
   `Nom_planning` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `Planning`
+--
+
+TRUNCATE TABLE `Planning`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Utilisateur`
 --
 
+DROP TABLE IF EXISTS `Utilisateur`;
 CREATE TABLE `Utilisateur` (
   `id` int(10) UNSIGNED NOT NULL,
   `Nom` varchar(45) NOT NULL,
@@ -241,6 +355,18 @@ CREATE TABLE `Utilisateur` (
   `NomConnection` varchar(32) NOT NULL,
   `MotDePasse` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `Utilisateur`
+--
+
+TRUNCATE TABLE `Utilisateur`;
+--
+-- Dumping data for table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`id`, `Nom`, `Prenom`, `Age`, `Type`, `NomConnection`, `MotDePasse`) VALUES
+(1, 'to', 'to', 20, 'élève', 'toto', '0000');
 
 --
 -- Indexes for dumped tables
@@ -328,7 +454,7 @@ ALTER TABLE `Lie3`
 -- Indexes for table `Lie5`
 --
 ALTER TABLE `Lie5`
-  ADD KEY `Lie5_CompSecUtil_idx` (`idCompSec`),
+  ADD KEY `Lie5_CompSecUtil_idx` (`idCompSecUtil`),
   ADD KEY `Lie5_Critere_idx` (`idCritere`);
 
 --
@@ -339,15 +465,15 @@ ALTER TABLE `Lie6`
   ADD KEY `Lie6_Planning_idx` (`idPlaning`);
 
 --
--- Indexes for table `Modele_Comp_Prin`
+-- Indexes for table `modele_comp_prin`
 --
-ALTER TABLE `Modele_Comp_Prin`
+ALTER TABLE `modele_comp_prin`
   ADD PRIMARY KEY (`idModCompPrin`);
 
 --
--- Indexes for table `Modele_Comp_Sec`
+-- Indexes for table `modele_comp_sec`
 --
-ALTER TABLE `Modele_Comp_Sec`
+ALTER TABLE `modele_comp_sec`
   ADD PRIMARY KEY (`idModCompSec`);
 
 --
@@ -398,15 +524,15 @@ ALTER TABLE `Grille_de_competence_APP`
 ALTER TABLE `Groupe`
   MODIFY `id_groupe` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `Modele_Comp_Prin`
+-- AUTO_INCREMENT for table `modele_comp_prin`
 --
-ALTER TABLE `Modele_Comp_Prin`
-  MODIFY `idModCompPrin` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `modele_comp_prin`
+  MODIFY `idModCompPrin` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `Modele_Comp_Sec`
+-- AUTO_INCREMENT for table `modele_comp_sec`
 --
-ALTER TABLE `Modele_Comp_Sec`
-  MODIFY `idModCompSec` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `modele_comp_sec`
+  MODIFY `idModCompSec` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `Planning`
 --
@@ -416,7 +542,7 @@ ALTER TABLE `Planning`
 -- AUTO_INCREMENT for table `Utilisateur`
 --
 ALTER TABLE `Utilisateur`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -481,7 +607,7 @@ ALTER TABLE `Lie3`
 -- Constraints for table `Lie5`
 --
 ALTER TABLE `Lie5`
-  ADD CONSTRAINT `Lie5_CompSec` FOREIGN KEY (`idCompSec`) REFERENCES `Competence_secondaire` (`id_comp_second`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Lie5_CompSecUtil` FOREIGN KEY (`idCompSecUtil`) REFERENCES `Competence_secondaire` (`id_comp_princ`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Lie5_Critere` FOREIGN KEY (`idCritere`) REFERENCES `Critere` (`id_critere`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
@@ -490,12 +616,6 @@ ALTER TABLE `Lie5`
 ALTER TABLE `Lie6`
   ADD CONSTRAINT `Lie6_Groupe` FOREIGN KEY (`idGroupe`) REFERENCES `Groupe` (`id_groupe`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Lie6_Planning` FOREIGN KEY (`idPlaning`) REFERENCES `Planning` (`id_planning`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `Modele_Comp_Sec`
---
-ALTER TABLE `Modele_Comp_Sec`
-  ADD CONSTRAINT `ModCompSec_ModCompPrin` FOREIGN KEY (`idModCompSec`) REFERENCES `Modele_Comp_Prin` (`idModCompPrin`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
