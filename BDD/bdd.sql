9c6c1bce0f68a78f2862bb6c78e16fbc8734a176
UBotella <ugo.botella@isep.fr>
2017-05-30 14:37:12 +0200

    Update bdd.sql
     Correction diverses + donnée designer

--- BDD/bdd.sql
+++ BDD/bdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 23, 2017 at 03:07 PM
+-- Generation Time: May 30, 2017 at 02:34 PM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `komodo`
 --
-CREATE DATABASE IF NOT EXISTS `komodo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `komodo`;
 
 -- --------------------------------------------------------
 
@@ -28,6 +26,7 @@ USE `komodo`;
 -- Table structure for table `Appartient`
 --
 
+DROP TABLE IF EXISTS `Appartient`;
 CREATE TABLE `Appartient` (
   `idEleve` int(10) UNSIGNED NOT NULL,
   `idGroupe` int(10) UNSIGNED NOT NULL,
@@ -35,33 +34,25 @@ CREATE TABLE `Appartient` (
   `datecrea` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Appartient`
---
-
-TRUNCATE TABLE `Appartient`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Competence_principale`
 --
 
+DROP TABLE IF EXISTS `Competence_principale`;
 CREATE TABLE `Competence_principale` (
   `id_comp` int(10) UNSIGNED NOT NULL,
   `Nom_competence_principale` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Competence_principale`
---
-
-TRUNCATE TABLE `Competence_principale`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Competence_secondaire`
 --
 
+DROP TABLE IF EXISTS `Competence_secondaire`;
 CREATE TABLE `Competence_secondaire` (
   `id_comp_second` int(10) UNSIGNED NOT NULL,
   `id_comp_princ` int(10) UNSIGNED NOT NULL,
@@ -70,17 +61,13 @@ CREATE TABLE `Competence_secondaire` (
   `Critere` varchar(650) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Competence_secondaire`
---
-
-TRUNCATE TABLE `Competence_secondaire`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Consulte`
 --
 
+DROP TABLE IF EXISTS `Consulte`;
 CREATE TABLE `Consulte` (
   `Evaluation` varchar(45) NOT NULL,
   `CommentaireGroupe` text NOT NULL,
@@ -89,17 +76,13 @@ CREATE TABLE `Consulte` (
   `idCom_Sec_Util` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Consulte`
---
-
-TRUNCATE TABLE `Consulte`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Eleves`
 --
 
+DROP TABLE IF EXISTS `Eleves`;
 CREATE TABLE `Eleves` (
   `id_utilisateur` int(10) UNSIGNED NOT NULL,
   `Numero_eleve` int(10) UNSIGNED NOT NULL,
@@ -107,17 +90,13 @@ CREATE TABLE `Eleves` (
   `Specialite` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Eleves`
---
-
-TRUNCATE TABLE `Eleves`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Evalue`
 --
 
+DROP TABLE IF EXISTS `Evalue`;
 CREATE TABLE `Evalue` (
   `idEleve` int(10) UNSIGNED NOT NULL,
   `idGroupe` int(10) UNSIGNED NOT NULL,
@@ -128,28 +107,19 @@ CREATE TABLE `Evalue` (
   `CommentaireProfesseur` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Evalue`
---
-
-TRUNCATE TABLE `Evalue`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Grille_de_competence_APP`
 --
 
+DROP TABLE IF EXISTS `Grille_de_competence_APP`;
 CREATE TABLE `Grille_de_competence_APP` (
   `id_grille` int(10) UNSIGNED NOT NULL,
   `Nom_grille` varchar(45) NOT NULL,
   `Promo` int(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Grille_de_competence_APP`
---
-
-TRUNCATE TABLE `Grille_de_competence_APP`;
 --
 -- Dumping data for table `Grille_de_competence_APP`
 --
@@ -164,6 +134,7 @@ INSERT INTO `Grille_de_competence_APP` (`id_grille`, `Nom_grille`, `Promo`) VALU
 -- Table structure for table `Groupe`
 --
 
+DROP TABLE IF EXISTS `Groupe`;
 CREATE TABLE `Groupe` (
   `id_groupe` int(10) UNSIGNED NOT NULL,
   `Nom` varchar(45) NOT NULL,
@@ -172,78 +143,58 @@ CREATE TABLE `Groupe` (
   `idGrilleAPP` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Groupe`
---
-
-TRUNCATE TABLE `Groupe`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie2`
 --
 
+DROP TABLE IF EXISTS `Lie2`;
 CREATE TABLE `Lie2` (
   `idEleve` int(10) UNSIGNED NOT NULL,
   `idGrilleComp` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Lie2`
---
-
-TRUNCATE TABLE `Lie2`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie3`
 --
 
+DROP TABLE IF EXISTS `Lie3`;
 CREATE TABLE `Lie3` (
   `idGrilleComp` int(10) UNSIGNED NOT NULL,
   `idCompPrin` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Lie3`
---
-
-TRUNCATE TABLE `Lie3`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Lie6`
 --
 
+DROP TABLE IF EXISTS `Lie6`;
 CREATE TABLE `Lie6` (
   `idGroupe` int(10) UNSIGNED NOT NULL,
   `idPlaning` int(10) UNSIGNED NOT NULL,
   `Descriptif` text NOT NULL,
   `Date` date NOT NULL,
-  `Heure` time DEFAULT NULL
+  `Debut` time DEFAULT NULL,
+  `Fin` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Lie6`
---
-
-TRUNCATE TABLE `Lie6`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Modele_Comp_Prin`
 --
 
+DROP TABLE IF EXISTS `Modele_Comp_Prin`;
 CREATE TABLE `Modele_Comp_Prin` (
   `idModCompPrin` int(10) UNSIGNED NOT NULL,
   `Nom` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Modele_Comp_Prin`
---
-
-TRUNCATE TABLE `Modele_Comp_Prin`;
 --
 -- Dumping data for table `Modele_Comp_Prin`
 --
@@ -258,39 +209,32 @@ INSERT INTO `Modele_Comp_Prin` (`idModCompPrin`, `Nom`) VALUES
 -- Table structure for table `Modele_Comp_Sec`
 --
 
+DROP TABLE IF EXISTS `Modele_Comp_Sec`;
 CREATE TABLE `Modele_Comp_Sec` (
   `idModCompSec` int(10) UNSIGNED NOT NULL,
-  `ponderation` int(11) NOT NULL,
   `Nom` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Modele_Comp_Sec`
---
-
-TRUNCATE TABLE `Modele_Comp_Sec`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Planning`
 --
 
+DROP TABLE IF EXISTS `Planning`;
 CREATE TABLE `Planning` (
   `id_planning` int(10) UNSIGNED NOT NULL,
-  `Nom_planning` varchar(45) NOT NULL
+  `Nom_planning` varchar(45) NOT NULL,
+  `Description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Planning`
---
-
-TRUNCATE TABLE `Planning`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Utilisateur`
 --
 
+DROP TABLE IF EXISTS `Utilisateur`;
 CREATE TABLE `Utilisateur` (
   `id` int(10) UNSIGNED NOT NULL,
   `Nom` varchar(45) NOT NULL,
@@ -301,11 +245,6 @@ CREATE TABLE `Utilisateur` (
   `MotDePasse` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Truncate table before insert `Utilisateur`
---
-
-TRUNCATE TABLE `Utilisateur`;
 --
 -- Indexes for dumped tables
 --
@@ -364,9 +303,9 @@ ALTER TABLE `Grille_de_competence_APP`
 --
 ALTER TABLE `Groupe`
   ADD PRIMARY KEY (`id_groupe`),
-  ADD UNIQUE KEY `Groupe_grilleAPP_idx` (`idGrilleAPP`),
   ADD KEY `Groupe_prof_idx` (`idClient`),
-  ADD KEY `Groupe_tuteur_idx` (`idTuteur`);
+  ADD KEY `Groupe_tuteur_idx` (`idTuteur`),
+  ADD KEY `Groupe_grilleAPP_idx` (`idGrilleAPP`) USING BTREE;
 
 --
 -- Indexes for table `Lie2`
@@ -506,7 +445,7 @@ ALTER TABLE `Evalue`
 --
 ALTER TABLE `Groupe`
   ADD CONSTRAINT `Groupe_client` FOREIGN KEY (`idClient`) REFERENCES `Utilisateur` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `Groupe_grilleAPP` FOREIGN KEY (`id_groupe`) REFERENCES `Grille_de_competence_APP` (`id_grille`),
+  ADD CONSTRAINT `Groupe_grilleAPP` FOREIGN KEY (`idGrilleAPP`) REFERENCES `Grille_de_competence_APP` (`id_grille`),
   ADD CONSTRAINT `Groupe_tuteur` FOREIGN KEY (`idTuteur`) REFERENCES `Utilisateur` (`id`) ON UPDATE CASCADE;
 
 --
@@ -529,6 +468,100 @@ ALTER TABLE `Lie3`
 ALTER TABLE `Lie6`
   ADD CONSTRAINT `Lie6_Groupe` FOREIGN KEY (`idGroupe`) REFERENCES `Groupe` (`id_groupe`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Lie6_Planning` FOREIGN KEY (`idPlaning`) REFERENCES `Planning` (`id_planning`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+
+--
+-- Database: `phpmyadmin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma__pdf_pages`
+--
+-- Creation: May 09, 2017 at 11:37 AM
+--
+
+CREATE TABLE IF NOT EXISTS `pma__pdf_pages` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `page_nr` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `page_descr` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  PRIMARY KEY (`page_nr`),
+  KEY `db_name` (`db_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='PDF relation pages for phpMyAdmin';
+
+--
+-- Truncate table before insert `pma__pdf_pages`
+--
+
+TRUNCATE TABLE `pma__pdf_pages`;
+--
+-- Dumping data for table `pma__pdf_pages`
+--
+
+INSERT INTO `pma__pdf_pages` (`db_name`, `page_nr`, `page_descr`) VALUES
+('komodo', 1, 'komodo'),
+('komodo', 2, 'komodo_05_23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma__table_coords`
+--
+-- Creation: May 09, 2017 at 11:37 AM
+--
+
+CREATE TABLE IF NOT EXISTS `pma__table_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `pdf_page_number` int(11) NOT NULL DEFAULT '0',
+  `x` float UNSIGNED NOT NULL DEFAULT '0',
+  `y` float UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`db_name`,`table_name`,`pdf_page_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for phpMyAdmin PDF output';
+
+--
+-- Truncate table before insert `pma__table_coords`
+--
+
+TRUNCATE TABLE `pma__table_coords`;
+--
+-- Dumping data for table `pma__table_coords`
+--
+
+INSERT INTO `pma__table_coords` (`db_name`, `table_name`, `pdf_page_number`, `x`, `y`) VALUES
+('komodo', 'Appartient', 1, 1042, 236),
+('komodo', 'Appartient', 2, 1042, 236),
+('komodo', 'Competence_principale', 1, 65, 602),
+('komodo', 'Competence_principale', 2, 65, 602),
+('komodo', 'Competence_secondaire', 1, 74, 324),
+('komodo', 'Competence_secondaire', 2, 74, 324),
+('komodo', 'Consulte', 1, 106, 1),
+('komodo', 'Consulte', 2, 106, 1),
+('komodo', 'Eleves', 1, 1046, 71),
+('komodo', 'Eleves', 2, 1046, 71),
+('komodo', 'Evalue', 1, 485, 146),
+('komodo', 'Evalue', 2, 485, 146),
+('komodo', 'Grille_de_competence_APP', 1, 1061, 780),
+('komodo', 'Grille_de_competence_APP', 2, 1061, 780),
+('komodo', 'Groupe', 1, 1062, 510),
+('komodo', 'Groupe', 2, 1062, 510),
+('komodo', 'Lie2', 1, 1700, 293),
+('komodo', 'Lie2', 2, 1700, 293),
+('komodo', 'Lie3', 1, 405, 781),
+('komodo', 'Lie3', 2, 405, 781),
+('komodo', 'Lie6', 1, 518, 563),
+('komodo', 'Lie6', 2, 518, 563),
+('komodo', 'Modele_Comp_Prin', 1, 451, 946),
+('komodo', 'Modele_Comp_Prin', 2, 451, 946),
+('komodo', 'Modele_Comp_Sec', 1, 812, 951),
+('komodo', 'Modele_Comp_Sec', 2, 812, 951),
+('komodo', 'Planning', 1, 521, 435),
+('komodo', 'Planning', 2, 521, 435),
+('komodo', 'Utilisateur', 1, 1296, 368),
+('komodo', 'Utilisateur', 2, 1296, 368);
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
